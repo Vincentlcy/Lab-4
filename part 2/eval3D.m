ce3cf47f4f7296559dcff8be2d67a4c5af677d66
Chuyang Liu <chuyang1@cs.ualberta.ca>
2018-11-18 21:19:39 -0700

    a

--- part 2/eval3D.m
+++ part 2/eval3D.m
@@ -3,7 +3,7 @@ close all
 addpath /cshome/vis/data
 robot3D('new')
 load('human_data.mat')
-load('walk2.mat')
+load('walk1.mat')
 
 %different initials
 Lp = [-pi/8;0;-pi/16;pi/4];
@@ -20,5 +20,5 @@ for i=1:size(L,2)
    Rp = invKin3D(Mr,Rp,R(:,i));
    theta=[Lp;Rp];
    robot3D(humanInterp(drad,theta))
-   pause(0.02);
+   pause();
 end
